a2346af817d09e32bae4c2b65d9d52a9f1a1e9be
Jesse <jesse@guojiang.tv>
2019-03-22 15:40:40 +0800

    v2

--- webpack-dll.config.js
+++ webpack-dll.config.js
@@ -23,21 +23,21 @@ if(isPc){
     baseEntryDir = './national-overseas/src/v2/pc/';
     entryDir = baseEntryDir + '**/*.js';
     outputDir = path.resolve(__dirname, './national-overseas/dist/v2/pc/');
-    outputPublicDir = '//static.cblive.tv/dist/pc/';
+    outputPublicDir = '//static.cblive.tv/dist/v2/pc/';
     entries = ['vue','axios','layer','jquery','service'];
     dll_manifest_name = 'dll_pc_manifest';
 }else if(isVn){
     baseEntryDir = './national-overseas/src/vietnam/mobile/';
     entryDir = baseEntryDir + '**/*.js';
     outputDir = path.resolve(__dirname, './national-overseas/dist/vietnam/mobile/');
-    outputPublicDir = '//static.cblive.tv/dist/vietnam/mobile/';
+    outputPublicDir = '//static.cblive.tv/dist/v2/vietnam/mobile/';
     entries = ['vue', 'axios', 'layer','service'];
     dll_manifest_name = 'dll_vn_manifest';
 }else{
     baseEntryDir = './national-overseas/src/v2/mobile/';
     entryDir = baseEntryDir + '**/*.*';
     outputDir = path.resolve(__dirname, './national-overseas/dist/v2/mobile/');
-    outputPublicDir = '//static.cblive.tv/dist/mobile/';
+    outputPublicDir = '//static.cblive.tv/dist/v2/mobile/';
     entries = ['vue', 'axios', 'layer','service'];
     dll_manifest_name = 'dll_manifest';
 }
